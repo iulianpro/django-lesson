96f1097b9e0247126c45c1c57b534f622a028986
iulianpro <iulian.prodea@gmail.com>
2020-04-22 01:39:33 +0100

    6th commit

--- django_lesson/urls.py
+++ django_lesson/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import get_todo_list
+from myapp.views import get_todo_list, create_an_item
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', get_todo_list)
+    url(r'^$', get_todo_list),
+    url(r'^add$', create_an_item)
 ]
